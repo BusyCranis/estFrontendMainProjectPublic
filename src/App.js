392383bd3c2d99d3bab92ae9c035758456baca41
cranis <cranis2@naver.com>
2024-12-18 10:36:12 +0900

    fewwfwfwefewf

--- src/App.js
+++ src/App.js
@@ -1,143 +1,143 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 import { Routes, Route, useNavigate, Link } from "react-router-dom";
-import { styled } from '@mui/material/styles';
-import Login from './component/Login';
-import UserSignPage from './component/userSignPage';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
+import { styled } from "@mui/material/styles";
+import Login from "./component/Login";
+import UserSignPage from "./component/userSignPage";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import HomeIcon from '@mui/icons-material/Home';
-import AddIcon from '@mui/icons-material/Add';
-import SettingsIcon from '@mui/icons-material/Settings';
+import HomeIcon from "@mui/icons-material/Home";
+import AddIcon from "@mui/icons-material/Add";
+import SettingsIcon from "@mui/icons-material/Settings";
 import { IconContext } from "react-icons";
-import { CgChevronLeftO } from 'react-icons/cg';
-import './App.css';
+import { CgChevronLeftO } from "react-icons/cg";
+import "./App.css";
 import styles from "./page.module.css";
-import MainList from './component/MainList';
-import zIndex from '@mui/material/styles/zIndex';
-
-
-
+import MainList from "./component/MainList";
+import zIndex from "@mui/material/styles/zIndex";
 
 function App() {
-
   const [movies, setMovies] = useState([]);
-  const [havemoney, sethavemoney] = useState(0)
+  const [havemoney, sethavemoney] = useState(0);
   const [buyinfo, setbuyinfo] = useState([]);
   const [soldinfo, setsoldinfo] = useState([]);
   const navLogin = useNavigate();
 
-  
-
   function addbuyinfo(newbought) {
-    setbuyinfo([...buyinfo, newbought])
+    setbuyinfo([...buyinfo, newbought]);
   }
 
   function addsoldinfo(newsold) {
-    setsoldinfo([...soldinfo, newsold])
+    setsoldinfo([...soldinfo, newsold]);
   }
 
   const [signeduserId, setsigneduserId] = useState([
-    { accountName: 'a', password: '1', id: 'hash' },
+    { accountName: "a", password: "1", id: "hash" },
   ]);
 
   const addMovie = (movie) => {
-    setMovies([
-      ...movies,
-      movie
-    ]);
+    setMovies([...movies, movie]);
   };
 
   const addUser = (user) => {
-    setsigneduserId([
-      ...signeduserId,
-      user
-    ]);
+    setsigneduserId([...signeduserId, user]);
   };
 
-  const additeminshop = (user) => {
-  };
+  const additeminshop = (user) => {};
 
   const addcharge = (chargeamount) => {
-    sethavemoney(havemoney + chargeamount)
-  }
+    sethavemoney(havemoney + chargeamount);
+  };
 
   const spendmoney = (spendamount) => {
-    sethavemoney(havemoney - spendamount)
-  }
+    sethavemoney(havemoney - spendamount);
+  };
 
   function goToMainPage() {
-    console.log("main!")
+    console.log("main!");
     navLogin("/");
   }
 
   function goToLoginPage() {
-    console.log("login!")
+    console.log("login!");
     navLogin("/login");
   }
 
-
-
-
   return (
-      <div   className={styles.container}  >
-        {/* <ButtonAppBar></ButtonAppBar> */}
-        {/* <SimpleBottomNavigation></SimpleBottomNavigation> */}
+    <div className={styles.container}>
+      {/* <ButtonAppBar></ButtonAppBar> */}
+      {/* <SimpleBottomNavigation></SimpleBottomNavigation> */}
 
-        <div  style={{minHeight: "60px", minWidth: "2000px", backgroundColor: "white", display: "flex", position: "fixed"}}>
-          <img   
-            src='/assetss/full-logo.png'
-            style={{maxHeight: "60px", maxWidth: "60px"}}
-            onClick={goToMainPage}
-          >
-          </img>
+      <div
+        style={{
+          minHeight: "60px",
+          minWidth: "2000px",
+          backgroundColor: "white",
+          display: "flex",
+          position: "fixed",
+        }}
+      >
+        <img
+          src="/assetss/full-logo.png"
+          style={{ maxHeight: "60px", maxWidth: "60px" }}
+          onClick={goToMainPage}
+        ></img>
 
-          <img   
-            src='/assetss/icon/icon-user.png'
-            style={{maxHeight: "60px", maxWidth: "60px"}}
-            onClick={goToLoginPage}
-          >
-          </img>
-        </div>
-
-        <br></br><br></br><br></br><br></br>
-
-        <Routes>
-          <Route path="/" element={<MainList   />} exact />
-          <Route path="/login" element={<Login />} exact />
-        </Routes>
-
-        <SimpleBottomNavigation></SimpleBottomNavigation>
+        <img
+          src="/assetss/icon/icon-user.png"
+          style={{ maxHeight: "60px", maxWidth: "60px" }}
+          onClick={goToLoginPage}
+        ></img>
       </div>
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <Routes>
+        <Route path="/" element={<MainList />} exact />
+        <Route path="/login" element={<Login />} exact />
+      </Routes>
+
+      <SimpleBottomNavigation></SimpleBottomNavigation>
+    </div>
   );
 }
 
-
 export function ButtonAppBar() {
-
   const navigate = useNavigate();
 
   return (
-
-    <div  style={{minHeight: "100px"}}>
-      <Box    style={{minHeight: "100px", minWidth: "600px", zIndex: 9999}}     sx={{ position: "fixed", flexGrow: 1 }}>
+    <div style={{ minHeight: "100px" }}>
+      <Box
+        style={{ minHeight: "100px", minWidth: "600px", zIndex: 9999 }}
+        sx={{ position: "fixed", flexGrow: 1 }}
+      >
         {/* <span onClick={() => navigate("/")}>Picocon</span> */}
-        {window.location.pathname === "/" ? "" :
+        {window.location.pathname === "/" ? (
+          ""
+        ) : (
           <div className="return">
-            <IconContext.Provider value={{ color: "#000000", className: "global-class-name", size: "3em" }}>
+            <IconContext.Provider
+              value={{
+                color: "#000000",
+                className: "global-class-name",
+                size: "3em",
+              }}
+            >
               <CgChevronLeftO onClick={() => navigate(-1)} />
             </IconContext.Provider>
           </div>
-        }
+        )}
       </Box>
     </div>
-
   );
 }
 
@@ -172,17 +172,11 @@ export function SimpleBottomNavigation() {
             label="Favorites"
             icon={<AddIcon />}
           ></BottomNavigationAction>
-          <BottomNavigationAction
-            
-            icon={<SettingsIcon />}
-          />
+          <BottomNavigationAction icon={<SettingsIcon />} />
         </BottomNavigation>
       </Box>
     </>
   );
 }
-
-
-
 
 export default App;
